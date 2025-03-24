54db3bd810193df55d314d5f0a6c97ea3ab34761
jonathan117lee <k1222168@students.katyisd.org>
2025-03-24 09:28:58 -0500

    a

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,39 @@
+import java.util.*;
 public class Player {
+
+    private int trains;
+    private int[] trainCards;
+    private ArrayList<Ticket> tickets;
+    
+    private ArrayList<Route> routes;
+    private int stations;
+
+    public void addRoute(Route r ) {
+
+
+    }
+    public void addTrainCard(int color) {
+
+    }
+    
+    
+    //sdfsdfsdf
+
+    public int getNumTrains() {
+        return trains;
+    }
+    public int[] getTrainCards() {
+        return trainCards;
+    }
+    public ArrayList<Ticket> getTickets() {
+        return tickets;
+    }
+    public int getPoints() {
+        for(int )
+    }
+    public ArrayList<Route> getRoutes() {
+        return routes;
+
+    }
 
 }
