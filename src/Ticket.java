e25baa51915197bc6aeb1e75ac10fa6fd2221c06
AZRL1 <ArellaZhangrl@gmail.com>
2025-03-28 09:31:50 -0500

    ticket

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,3 +1,18 @@
-public class Ticket {
-private String[] cities = new String[2];
+public class Ticket extends JPanel{
+    private String[] cities;
+    private int points;
+    private BufferedImage[] images = new BufferedImage[46];
+
+    public void paint(Graphics g)
+    {
+        
+    }
+    public String[] getCities()
+    {
+        return cities;
+    }
+    public int getPoints()
+    {
+        return points;
+    }
 }
