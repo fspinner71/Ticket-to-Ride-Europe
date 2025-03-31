ec1eaf0f7ce65cdfdd93494d8b27b297f0565bb7
AZRL1 <ArellaZhangrl@gmail.com>
2025-03-31 08:52:44 -0500

    City

---
+++ src/City.java
@@ -0,0 +1,28 @@
+public class City
+{
+    private String name;
+    private ArrayList<Route> routes;
+    private int x;
+    private int y;
+    private BufferedImage image;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public ArrayList<Route> getRoutes()
+    {
+        return routes;
+    }
+
+    public int getXCoord()
+    {
+        return x;
+    }
+    
+    public int getYCoord()
+    {
+        return y;
+    }
+}
