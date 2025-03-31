ac9d1d3856cfa8bdd8607aa95424a33dc67aa7b7
elilizabethian <elizabeth.lixin.jiang@gmail.com>
2025-03-31 09:25:29 -0500

    frame!

--- src/Frame.java
+++ src/Frame.java
@@ -1,9 +1,17 @@
-public class Frame {
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
     public static final int WIDTH = 1920;
     public static final int HEIGHT = 1080;
 
-    public Frame()
+    public Frame(String s)
     {
+        super(s);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(WIDTH, HEIGHT);
+		add(new GamePanel());
+		setVisible(true);
+        setResizable(false); 
 
     }
 }
