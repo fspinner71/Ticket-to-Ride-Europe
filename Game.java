90eb470cb245e18ed601a5b288970aab1f5dc0c0
elilizabethian <elizabeth.lixin.jiang@gmail.com>
2025-04-01 09:05:14 -0500

    game update

--- Game.java
+++ Game.java
@@ -5,5 +5,22 @@ public class Game {
     private City cities[];
     private Route routes[];
     private int currentPlayer;
-    private Stack ticket<Tickets>;
+    private Stack<Ticket> tickets;
+    private Stack<Integer> deck;
+    private int turn;
+    private int shouldEnd;
+    private boolean drawnOne;
+    public static final int RED = 0;
+    public static final int ORANGE = 1;
+    public static final int YELLOW = 2;
+    public static final int GREEN = 3;
+    public static final int BLUE = 4;
+    public static final int PINK = 5;
+    public static final int WHITE = 6;
+    public static final int BLACK = 7;
+    public static final int ANY = 8;
+
+    public static void drawCard(){
+        
+    }
 }
