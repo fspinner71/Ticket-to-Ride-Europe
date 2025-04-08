8aef76545a7642c84deb2b84eb7712afbd383b2d
AZRL1 <ArellaZhangrl@gmail.com>
2025-04-08 09:12:12 -0500

    buyStation comments

--- src/Player.java
+++ src/Player.java
@@ -71,15 +71,15 @@ public class Player {
 //         }
 //         return false;
 //     }
-    public boolean buyStation(int traincard1) { // if thir d
+    public boolean buyStation(int traincard1) { 
         int numNeeded = 0;
-        if(stations == 3)
+        if(stations == 3)//if building first station
              numNeeded = 1;
-        else if(stations == 2)
+        else if(stations == 2)//if building second station
              numNeeded = 2;
-        else if(stations == 1)
+        else if(stations == 1)//if building third/last station
              numNeeded = 3;
-        if(trainCards[traincard1] >= numNeeded ) { //idk waht this is
+        if(trainCards[traincard1] >= numNeeded ) { //check if player has enough cards to build
             for(int i = 0; i < numNeeded; i++)
             {
                 trainCards[traincard1]--;
