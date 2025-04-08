1b2d314572cbded969ae3786629cd97b2de3133f
AZRL1 <ArellaZhangrl@gmail.com>
2025-04-08 09:08:39 -0500

    new buyStation

--- src/Player.java
+++ src/Player.java
@@ -29,48 +29,66 @@ public class Player {
     public void addTicket(Ticket a) {
         tickets.add(a);
     }
-    public boolean buyStation(int traincard1) { //if first station
-        if(trainCards[traincard1] > 0) {
-            trainCards[traincard1]--;
-            stations--;
-            return true; 
-        }
-        return false;
+//     public boolean buyStation(int traincard1) { //if first station
+//         if(trainCards[traincard1] > 0) {
+//             trainCards[traincard1]--;
+//             stations--;
+//             return true; 
+//         }
+//         return false;
         
-    }
-    public boolean buyStation(int traincard1, int traincard2) { //if seocnd
-        if(trainCards[traincard1] > 0 ) {
-            trainCards[traincard1]--;
-            if(trainCards[traincard2] > 0 && traincard2 == traincard1) {
-                trainCards[traincard2]--;
-                stations--;
-            return true; 
-        }
-        trainCards[traincard1]++;
-        return false;
-    }
-    return false;
-}
-    public boolean buyStation(int traincard1, int traincard2, int traincard3) { // if thir d
-        if(trainCards[traincard1] > 0 ) { //idk waht this is
-            trainCards[traincard1]--;
-            if(trainCards[traincard2] > 0 && traincard2 == traincard1) {
-                trainCards[traincard2]--; 
+//     }
+//     public boolean buyStation(int traincard1, int traincard2) { //if seocnd
+//         if(trainCards[traincard1] > 0 ) {
+//             trainCards[traincard1]--;
+//             if(trainCards[traincard2] > 0 && traincard2 == traincard1) {
+//                 trainCards[traincard2]--;
+//                 stations--;
+//             return true; 
+//         }
+//         trainCards[traincard1]++;
+//         return false;
+//     }
+//     return false;
+// }
+//     public boolean buyStation(int traincard1, int traincard2, int traincard3) { // if thir d
+//         if(trainCards[traincard1] > 0 ) { //idk waht this is
+//             trainCards[traincard1]--;
+//             if(trainCards[traincard2] > 0 && traincard2 == traincard1) {
+//                 trainCards[traincard2]--; 
             
-                if(trainCards[traincard3] > 0 && traincard3 == traincard2) {
-                    trainCards[traincard3]--; 
-                    stations--;
-                return true; 
-                }
-            trainCards[traincard2]++;
-            trainCards[traincard1]++;
-            return false;
+//                 if(trainCards[traincard3] > 0 && traincard3 == traincard2) {
+//                     trainCards[traincard3]--; 
+//                     stations--;
+//                 return true; 
+//                 }
+//             trainCards[traincard2]++;
+//             trainCards[traincard1]++;
+//             return false;
+//             }
+//             trainCards[traincard1]++;
+//             return false;
+//         }
+//         return false;
+//     }
+    public boolean buyStation(int traincard1) { // if thir d
+        int numNeeded = 0;
+        if(stations == 3)
+             numNeeded = 1;
+        else if(stations == 2)
+             numNeeded = 2;
+        else if(stations == 1)
+             numNeeded = 3;
+        if(trainCards[traincard1] >= numNeeded ) { //idk waht this is
+            for(int i = 0; i < numNeeded; i++)
+            {
+                trainCards[traincard1]--;
             }
-            trainCards[traincard1]++;
-            return false;
+            return true;
         }
         return false;
     }
+
     
     public boolean buyRoute(Route p, int locomotivesused, int buyingcolor) { //buying color is what color u tryna buy with if its a grey route
         //we will make sure buying color is same as route color if route has a color in the game class or wtv so they cant buy like a red route with a blue card
